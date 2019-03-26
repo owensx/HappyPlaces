7b40b48b85b1d74161320d36fcfe1f5c2d0fc486
owensx <xavier.owens0@gmail.com>
2019-03-26 03:55:49 +0000

    test

--- src/settings.py
+++ src/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -84,3 +85,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+ALLOWED_HOSTS = ['ec2-18-221-164-96.us-east-2.compute.amazonaws.com']
