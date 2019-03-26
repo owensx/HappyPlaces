ee00c9298e7ce126cbe66c4ff12a7e04cab9013a
owensx <xavier.owens0@gmail.com>
2019-03-26 01:25:33 +0000

    adding db details

---
+++ src/settings.py
@@ -0,0 +1,86 @@
+"""
+Django settings for HappyPlace project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '1($oqr)g%1n$43_m$dj!+1e8zt90n5uuq@2cr5o^^i$xk%g#5b'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []  
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'src.site',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'src.urls'
+
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'HappyPlaces',
+        'USER': 'happyplacesdev',
+        'PASSWORD': 'happyplacesdev',
+        'HOST': 'happyplacesdbdev.ca3z3ga9nwuc.us-east-2.rds.amazonaws.com',
+        'PORT': 3306,
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
---
+++ src/urls.py
@@ -0,0 +1,30 @@
+from django.urls import *
+from src.site import views
+
+urlpatterns = [
+    path('getHappyPlaceById/<happyPlaceId>', views.getHappyPlaceById)
+]#'',
+    # Examples:
+    # url(r'^$', 'src.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+#    url(r'^admin/', include(admin.site.urls)),
+    
+    #url(r'^happyPlace/(?P<happyPlaceId>\d+)', 'src.site.views.HappyPlaceView', name='viewHappyPlace'),
+  #  url(r'^submit/', 'src.site.views.Submit', name='submit'),
+#     url(r'^submitState/', 'src.site.views.SubmitState', name='submitState'),
+#     url(r'^submitCityForState/(?P<stateId>[0-9]*)/$', 'src.site.views.SubmitCityForState', name='submitCityForState'),
+#     url(r'^submitHappyPlaceForCity/(?P<cityId>[0-9]*)/$', 'src.site.views.SubmitHappyPlaceForCity', name='submitHappyPlaceForCity'),
+#     url(r'^submitHappyHourForHappyPlace/(?P<happyPlaceId>[0-9]*)/$', 'src.site.views.SubmitHappyHourForHappyPlace', name='submitHappyHourForHappyPlace'),
+ #   url(r'^error/', 'src.site.views.Error', name='error'),
+  #  url(r'^getNeighborhoods/(?P<cityToSearch>\w+)', 'src.site.utils.getNeighborhoodsForCity', name='getNeighborhoodsForCity'),
+ #   url(r'^getHappyPlaces/(?P<neighborhoodToSearch>\w+)', 'src.site.utils.getHappyPlacesForNeighborhood', name='getHappyPlacesForNeighborhood'),
+
+  #  url(r'^getHappyPlace/(?P<happyPlaceId>\w+)', 'src.site.utils.getHappyPlace', name='getHappyPlace'),
+
+ #   url(r'^getPhotos/(?P<location>.+\/.+)', 'src.site.utils.getPhotos', name = 'getPhotos'),
+ #   url(r'^getPlaceId/(?P<queryString>.+)', 'src.site.utils.getPlaceId', name = 'getPlaceId'),
+ #   url(r'^', 'src.site.views.Home', name='home'),
+        
+#] + staticfiles_urlpatterns()
+
