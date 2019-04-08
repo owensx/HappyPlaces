1adc91ae0f059d685024f873b1e030cb16c0ac96
owensx <xavier.owens0@gmail.com>
2019-04-08 04:21:03 +0000

    test

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,6 @@ COPY . /code/
 EXPOSE 8000
 
 RUN pip install -r requirements.txt
-RUN python src/manage.py migrate src.site
+RUN python src/manage.py migrate
 
 CMD ["python", "src/manage.py", "runserver", "0.0.0.0:8000"]
--- src/site/models.py
+++ src/site/models.py
@@ -1,51 +1,13 @@
 from django.db import models
-from django.forms import ModelForm
-from django import forms
-from datetime import datetime, timedelta
 from django.core import serializers
 from django.http.response import HttpResponse
 
-from src.site.templatetags.filters import *
-
-DAYS = (('S', 'Sunday')
-        , ('M', 'Monday')
-        , ('T', 'Tuesday')
-        , ('W', 'Wednesday')
-        , ('R', 'Thursday')
-        , ('F', 'Friday')
-        , ('Y', 'Saturday'))
-
-ZONES = (('-8', 'Pacific')
-         , ('-7', 'Mountain')
-         , ('-6', 'Central')
-         , ('-5', 'Eastern'))
-
-
-class State(models.Model):
-    id = models.IntegerField(primary_key=True)
-    timeUpdated = models.DateTimeField()
-
-    # required fields
-    name = models.CharField(max_length=50)
-    offset = models.IntegerField(choices=ZONES)
-
-    def __str__(self):
-        return self.name.__str__()
-
-    class Meta:
-        ordering = ('name',)
-        db_table = "state"
-
 
 class City(models.Model):
-    id = models.IntegerField(primary_key=True)
-    timeUpdated = models.DateTimeField()
-
-    # foreign keys
-    state = models.ForeignKey(State, related_name='cities', on_delete=models.DO_NOTHING)
-
     # required fields
     name = models.CharField(max_length=50)
+
+    time_updated = models.DateTimeField()
 
     def __str__(self):
         return self.name.__str__()
@@ -56,14 +18,13 @@ class City(models.Model):
 
 
 class Neighborhood(models.Model):
-    id = models.IntegerField(primary_key=True)
-    timeUpdated = models.DateTimeField()
-
     # foreign keys
-    city = models.ForeignKey(City, related_name='neighborhoods', on_delete=models.DO_NOTHING)
+    city = models.ForeignKey(City, related_name='neighborhoods', on_delete=models.PROTECT)
 
     # required fields
     name = models.CharField(max_length=50)
+
+    time_updated = models.DateTimeField()
 
     def __str__(self):
         return self.name.__str__()
@@ -74,12 +35,8 @@ class Neighborhood(models.Model):
 
 
 class HappyPlace(models.Model):
-    place_id = models.CharField(max_length=50, primary_key=True)
-    id = models.IntegerField(unique=True)
-    timeUpdated = models.DateTimeField()
-
     # foreign keys
-    neighborhood = models.ForeignKey(Neighborhood, related_name='happyPlaces', on_delete=models.DO_NOTHING)
+    neighborhood = models.ForeignKey(Neighborhood, related_name='happy_places', on_delete=models.PROTECT)
 
     # required fields
     name = models.CharField(max_length=50)
@@ -92,44 +49,22 @@ class HappyPlace(models.Model):
     latitude = models.FloatField(null=True)
     longitude = models.FloatField(null=True)
     price_level = models.IntegerField(null=True)
+    google_place_id = models.CharField(max_length=50, null=True)
 
+    time_updated = models.DateTimeField()
     active = models.BooleanField(default=False)
-    googled = models.BooleanField(default=False)
 
-    def getLatLng(self):
+    @property
+    def lat_lng(self):
         return {'lat': self.latitude, 'lng': self.longitude}
 
-    latLng = property(getLatLng)
-
-    def getMarkerInfo(self):
+    @property
+    def marker_info(self):
         return list([
             self.name
-            , self.latLng
+            , self.lat_lng
             , self.todaysSpecials
         ])
-
-    markerInfo = property(getMarkerInfo)
-
-    def getTodaysSpecials(self):
-        today = intToDayOfWeek((datetime.utcnow() + timedelta(hours=self.neighborhood.city.state.offset)).weekday())
-        happyHours = filter(lambda happyHour: today in happyHour.days, self.happyHours.all())
-
-        specials = []
-
-        for happyHour in happyHours:
-            displayNotes = '' if happyHour.display_notes == None else happyHour.display_notes
-            specials.append([formatTime(happyHour.start), formatTime(happyHour.end), displayNotes, [
-                ['beer', '' if happyHour.beer == None else happyHour.beer]
-                , ['wine_glass', '' if happyHour.wine_glass == None else happyHour.wine_glass]
-                , ['wine_bottle', '' if happyHour.wine_bottle == None else happyHour.wine_bottle]
-                , ['well', '' if happyHour.well == None else happyHour.well]
-                , ['shot_beer', '' if happyHour.shot_beer == None else happyHour.shot_beer]
-            ]
-                             ])
-
-        return specials
-
-    todaysSpecials = property(getTodaysSpecials)
 
     def render_to_response(self):
         data = serializers.serialize('json', self.get_queryset())
@@ -144,14 +79,13 @@ class HappyPlace(models.Model):
 
 
 class HappyHour(models.Model):
-    id = models.IntegerField(primary_key=True)
-    timeUpdated = models.DateTimeField()
-
     # foreign keys
-    happyPlace = models.ForeignKey(HappyPlace, to_field='id', related_name='happyHours', on_delete=models.DO_NOTHING)
+    happy_place = models.ForeignKey(HappyPlace, related_name='happy_hours', on_delete=models.CASCADE)
 
+    # required fields
     notes = models.CharField(max_length=200)
-    days = models.CharField(max_length=100, choices=DAYS)
+
+    # optional fields
     start = models.TimeField()
     end = models.TimeField()
     beer = models.CharField(max_length=200, null=True)
@@ -160,96 +94,15 @@ class HappyHour(models.Model):
     well = models.CharField(max_length=200, null=True)
     shot_beer = models.CharField(max_length=200, null=True)
     display_notes = models.CharField(max_length=200, null=True)
+    sunday = models.BooleanField(default=False)
+    monday = models.BooleanField(default=False)
+    tuesday = models.BooleanField(default=False)
+    wednesday = models.BooleanField(default=False)
+    thursday = models.BooleanField(default=False)
+    friday = models.BooleanField(default=False)
+    saturday = models.BooleanField(default=False)
+
+    time_updated = models.DateTimeField()
 
     class Meta:
         db_table = "happyhour"
-
-
-class Report(models.Model):
-    reportName = models.CharField(max_length=200)
-    lastRun = models.DateTimeField()
-
-
-class StateForm(ModelForm):
-    state = forms.ModelChoiceField(queryset=State.objects.all().order_by('name'))
-
-    class Meta:
-        model = State
-        fields = ['state']
-
-
-class CityForm(ModelForm):
-    city = forms.ModelChoiceField(queryset=City.objects.all().order_by('name'), empty_label='Add New City',
-                                  required=False)
-
-    name = forms.CharField(max_length=200, required=False)
-
-    def is_valid(self):
-        if super(CityForm, self).is_valid():
-
-            if not (self.cleaned_data['name'] or self.cleaned_data['city']):
-                return False
-            return True
-
-        return False
-
-    class Meta:
-        model = City
-        fields = ['city', 'name']
-
-
-class HappyPlaceForm(ModelForm):
-    #     def __init__(self, *args, **kwargs):
-    #         neighborhood = Neighborhood.objects.filter(city=City.objects.get(id=kwargs.pop('cityId')))
-    #         super(HappyPlaceForm, self).__init__(*args, **kwargs)
-    #         self.fields['neighborhood'] = forms.ChoiceField(
-    #                 choices=[(happyPlace, happyPlace) for happyPlace in HappyPlace.objects.filter(neighborhood=neighborhood)]
-    #                 )
-
-    place_id = forms.CharField(max_length=200, required=False,
-                               error_messages={'unique': "We already have that HappyPlace!"})
-
-    name = forms.CharField(max_length=200, required=True)
-
-    neighborhood = forms.ModelChoiceField(queryset=Neighborhood.objects.all().order_by('name'),
-                                          empty_label='Add New Neighborhood', required=False)
-    neighborhoodName = forms.CharField(max_length=200, required=False)
-
-    address = forms.CharField(max_length=200, required=False)
-    cross = forms.CharField(max_length=200, required=False)
-    city = forms.ModelChoiceField(queryset=City.objects.all(), required=False)
-    latitude = forms.FloatField(required=False)
-    longitude = forms.FloatField(required=False)
-    site = forms.CharField(max_length=200, required=False)
-    phone = forms.CharField(max_length=200, required=False)
-
-    class Meta:
-        model = HappyPlace
-        fields = ['place_id', 'name', 'neighborhoodName', 'address', 'cross', 'city', 'latitude', 'longitude', 'site',
-                  'phone']
-
-
-class HappyHourForm(ModelForm):
-    days = forms.MultipleChoiceField(choices=DAYS, widget=forms.CheckboxSelectMultiple())
-
-    class Meta:
-        model = HappyHour
-        fields = ['start', 'end', 'notes']
-
-
-class HappyHourAdminForm(ModelForm):
-    display_notes = forms.CharField(required=False)
-    beer = forms.CharField(required=False)
-    well = forms.CharField(required=False)
-    cocktail = forms.CharField(required=False)
-    wine_glass = forms.CharField(required=False)
-    wine_bottle = forms.CharField(required=False)
-    shot_beer = forms.CharField(required=False)
-    bar = forms.CharField(required=False)
-    snacks = forms.CharField(required=False)
-
-    days = forms.MultipleChoiceField(choices=DAYS, widget=forms.CheckboxSelectMultiple())
-
-    class Meta:
-        model = HappyHour
-        fields = ['start', 'end', 'notes']
